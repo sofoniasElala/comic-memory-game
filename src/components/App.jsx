30e0dac9d5e440b5c02fe66f411d5cb1df501c48
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-04-28 20:42:27 -0400

    move files into their appropriate folders

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import '../styles/App.css'
+import Cards from './characterCards'
+
+function App() {
+
+  return (
+    <>
+      <h1>MARVEL</h1>
+      <Cards />
+    </>
+  )
+}
+
+export default App
