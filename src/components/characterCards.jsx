27ac894bea9d24df9a8ecbb6cd4948b42bb25f68
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-04-28 20:39:28 -0400

    refactor and add styling

--- src/components/characterCards.jsx
+++ src/components/characterCards.jsx
@@ -1,33 +1,80 @@
 import { useEffect, useState } from "react";
-export default function Cards(){
-    const [charactersInfo, setCharactersInfo] = useState([]);
+import '../styles/characterCards.css'
+export default function Cards() {
+  const [charactersInfo, setCharactersInfo] = useState([]);
+  const [visibleCharacters, setVisibleCharacters] = useState([]);
 
-     //16542 - deadpool(2012 -15) 15 c, 20508 - spider man (16 - 18) 12 c, 22547 avengers (16 - 18) 30 c,
-     // const seriesId = [16542, 20508, 22547]
-    useEffect(() => {
-        marvelAPIHandler(setCharactersInfo);
-    }, [])
+  //16542 - deadpool(2012 -15) 16 c, 20508 - spider man (16 - 18) 12 c, 22547 avengers (16 - 18) 30 c,
+  // const seriesId = [16542, 20508, 22547]
+  useEffect(() => {
+    marvelAPIHandler(setCharactersInfo, setVisibleCharacters);
+  }, []);
 
-    return charactersInfo.map(character => {
-        return <div key={character.id} className="character">
-            <img src={character.url} alt="" />
-            <h2>{character.name}</h2>
-        </div>
-    })
+  return visibleCharacters.map((character) => {
+    return (
+      <div
+        key={character.id}
+        onClick={() =>
+          randomCharactersToDisplay(charactersInfo, setVisibleCharacters)
+        }
+        className="character"
+      >
+        <img src={character.url} alt="" />
+        <h5>{character.name}</h5>
+      </div>
+    );
+  });
 }
 
-function extractCharacters(results){
-    console.log(results);
-    return results.map(character => {
-        return {id: character.id, name: character.name, url: `${character.thumbnail.path}/portrait_incredible.${character.thumbnail.extension}`}
-    });
+function randomCharactersToDisplay(
+  charactersInfo,
+  setVisibleCharacters
+) {
+  const charactersIndex = [];
+  while (charactersIndex.length < 5) {
+    const randomIndex = Math.floor(Math.random() * charactersInfo.length);
+    if (charactersIndex.length > 0) {
+      if (
+        !charactersIndex.includes(randomIndex) &&
+        !charactersInfo[randomIndex].url.includes("image_not_available")
+      )
+        charactersIndex.push(randomIndex);
+    } else charactersIndex.push(randomIndex);
+  }
+  setVisibleCharacters(charactersIndex.map((i) => charactersInfo[i]));
 }
 
-function marvelAPIHandler(setCharactersInfo){
-    const MARVEL_K = import.meta.env.VITE_MARVEL;
+function extractCharacters(results) {
+  console.log(results);
+  return results.map((character) => {
+    return {
+      id: character.id,
+      name: character.name,
+      url: `${character.thumbnail.path}/portrait_incredible.${character.thumbnail.extension}`,
+    };
+  });
+}
 
-    fetch(`https://gateway.marvel.com/v1/public/series/22547/characters?apikey=${MARVEL_K}`)
-    .then(response => response.json())
-    .then(dataJSON => setCharactersInfo(extractCharacters(dataJSON.data.results)))
-    .catch(error => alert(error));
+async function marvelAPIHandler(setCharactersInfo, setVisibleCharacters) {
+  const MARVEL_K = import.meta.env.VITE_MARVEL;
+  const seriesId = [
+    16542,
+    20508,
+    22547,
+  ];
+  const randomSeriesId = Math.floor(Math.random() * 3);
+  try {
+    const response = await fetch(
+      `https://gateway.marvel.com/v1/public/series/${seriesId[randomSeriesId]}/characters?apikey=${MARVEL_K}`
+    );
+    const responseJSON = await response.json();
+    const extractedInfo = extractCharacters(responseJSON.data.results);
+    setCharactersInfo(extractedInfo);
+    randomCharactersToDisplay(
+      extractedInfo,
+      setVisibleCharacters
+    );
+  } catch (error) {
+    alert(error);
+  }
 }
